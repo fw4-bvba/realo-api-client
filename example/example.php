3fcce723bae9c69f662f56ac58ddabcc2ec830dc
Sam Feyaerts <sam@fw4.be>
2024-04-22 16:59:37 +0200

    style: update code style

--- example/example.php
+++ example/example.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Realo\Api\RealoApi;
@@ -6,8 +7,8 @@ use Realo\Api\RealoApiException;
 
 $api = RealoApi::create('YOUR_PUBLIC_KEY', 'YOUR_PRIVATE_KEY');
 try {
-	$response = $api->request('/agencies', 'GET');
-	var_dump($response);
+    $response = $api->request('/agencies', 'GET');
+    var_dump($response);
 } catch (RealoApiException $e) {
-	printf("Error %d: %s\n", $e->getCode(), $e->getMessage());
+    printf("Error %d: %s\n", $e->getCode(), $e->getMessage());
 }
--- src/RealoApiException.php
+++ src/RealoApiException.php
@@ -1,59 +1,60 @@
 <?php
+
 namespace Realo\Api;
 
 use GuzzleHttp\Exception\RequestException;
-use function GuzzleHttp\json_decode;
+use GuzzleHttp\Utils;
 
 class RealoApiException extends \RuntimeException
 {
-	/**
-	 * @var string|null
-	 */
-	protected $type;
+    /**
+     * @var string|null
+     */
+    protected $type;
 
-	/**
-	 * @var array|null
-	 */
-	protected $errors;
+    /**
+     * @var array|null
+     */
+    protected $errors;
 
-	public function __construct(RequestException $previous)
-	{
-		// Get the exception information.
-		$response = $previous->getResponse();
-		$code = $previous->getCode();
-		$message = $previous->getMessage();
+    public function __construct(RequestException $previous)
+    {
+        // Get the exception information.
+        $response = $previous->getResponse();
+        $code = $previous->getCode();
+        $message = $previous->getMessage();
 
-		$this->errors = null;
-		$this->type = null;
+        $this->errors = null;
+        $this->type = null;
 
-		if ($response && preg_match('|^application/json|', $response->getHeader('Content-Type')[0])) {
-			$body = json_decode((string) $response->getBody(), true);
-			if (isset($body['errors'])) {
-				$this->errors = $body['errors'];
-			}
-		}
+        if ($response && preg_match('|^application/json|', $response->getHeader('Content-Type')[0])) {
+            $body = Utils::jsonDecode((string) $response->getBody(), true);
+            if (isset($body['errors'])) {
+                $this->errors = $body['errors'];
+            }
+        }
 
-		if ($this->errors[0]) {
-			$this->type = $this->errors[0]['type'];
-			$message = $this->errors[0]['type'] . ': ' . $this->errors[0]['message'];
-		}
+        if ($this->errors[0]) {
+            $this->type = $this->errors[0]['type'];
+            $message = $this->errors[0]['type'] . ': ' . $this->errors[0]['message'];
+        }
 
-		parent::__construct($message, $code, $previous);
-	}
+        parent::__construct($message, $code, $previous);
+    }
 
-	/**
-	 * @return string|null
-	 */
-	public function getType()
-	{
-		return $this->type;
-	}
+    /**
+     * @return string|null
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
 
-	/**
-	 * @return array|null
-	 */
-	public function getErrors()
-	{
-		return $this->errors;
-	}
+    /**
+     * @return array|null
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
 }
